74542c76d673d55278791c184ccbf7f66e76f025
Yevhenii Volokitin <hej1ow9h@gmail.com>
2024-05-07 22:30:17 +0200

    Create post system with sql

--- server.js
+++ server.js
@@ -10,7 +10,62 @@ const db = mysql.createConnection({
    host: "localhost",
    user: "root",
    password: "",
-   database: "signup",
+   database: "posts",
+});
+db.connect((err) => {
+   if (err) {
+      throw err;
+   }
+   console.log("Connected to database");
+   const createPostsTable = `
+      CREATE TABLE IF NOT EXISTS Posts (
+         id INT AUTO_INCREMENT PRIMARY KEY,
+         title VARCHAR(255),
+         content TEXT,
+         image_url VARCHAR(255),
+         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+   `;
+
+   db.query(createPostsTable, (err, result) => {
+      if (err) {
+         throw err;
+      }
+      console.log("Posts table created successfully");
+   });
+});
+app.post("/create", (req, res) => {
+   const { title, content, image_url } = req.body;
+   const sql =
+      "INSERT INTO Posts (`title`, `content`, `image_url`) VALUES (?, ?, ?)";
+   const values = [title, content, image_url];
+
+   db.query(sql, values, (err, result) => {
+      if (err) {
+         console.error("Error adding post: ", err);
+         res.status(500).json({
+            error: "An error occurred while adding the post",
+         });
+      } else {
+         console.log("Post added successfully");
+         res.status(200).json({ message: "Post added successfully" });
+      }
+   });
+});
+// Добавляем маршрут для получения всех постов
+app.get("/main", (req, res) => {
+   const sql = "SELECT * FROM Posts";
+
+   db.query(sql, (err, results) => {
+      if (err) {
+         console.error("Error fetching posts: ", err);
+         res.status(500).json({
+            error: "An error occurred while fetching posts",
+         });
+      } else {
+         res.status(200).json(results);
+      }
+   });
 });
 
 app.post("/signup", (req, res) => {
