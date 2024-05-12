00aa89ec971cd437d686a65d948cd5cbdc750ea3
Yevhenii Volokitin <hej1ow9h@gmail.com>
2024-05-12 22:46:29 +0200

    Done token creation and saving, left to do site validation and routing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(cors());
@@ -17,12 +18,14 @@ db.connect((err) => {
       throw err;
    }
    console.log("Connected to database");
+
    const createPostsTable = `
       CREATE TABLE IF NOT EXISTS Posts (
          id INT AUTO_INCREMENT PRIMARY KEY,
          title VARCHAR(255),
          content TEXT,
          image_url VARCHAR(255),
+         username VARCHAR(255),
          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )
    `;
@@ -34,11 +37,12 @@ db.connect((err) => {
       console.log("Posts table created successfully");
    });
 });
+
 app.post("/create", (req, res) => {
-   const { title, content, image_url } = req.body;
+   const { title, content, image_url, username } = req.body;
    const sql =
-      "INSERT INTO Posts (`title`, `content`, `image_url`) VALUES (?, ?, ?)";
-   const values = [title, content, image_url];
+      "INSERT INTO Posts (`title`, `content`, `image_url`,`username`) VALUES (?, ?, ?, ?)";
+   const values = [title, content, image_url, username];
 
    db.query(sql, values, (err, result) => {
       if (err) {
@@ -79,20 +83,25 @@ app.post("/signup", (req, res) => {
    });
 });
 app.post("/login", (req, res) => {
+   const { email, password } = req.body;
    const sql = "SELECT * FROM login WHERE `email` = ? AND `password` = ?";
 
-   db.query(sql, [req.body.email, req.body.password], (err, data) => {
+   db.query(sql, [email, password], (err, data) => {
       if (err) {
-         return res.json("Error");
+         return res.status(500).json({ error: "Internal server error" });
       }
+
       if (data.length > 0) {
-         return res.json("Success");
+         // Генерация токена JWT
+         const token = jwt.sign({ email: email }, "your_secret_key", {
+            expiresIn: "1h",
+         });
+         return res.status(200).json({ token: token });
       } else {
-         return res.json("Failed");
+         return res.status(401).json({ error: "Authentication failed" });
       }
    });
 });
-
 app.listen(8081, () => {
    console.log("listening");
 });
